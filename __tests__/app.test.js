abfa1c4111cb445d3e81a5819c4da8dcdafdbe16
chrisRduckworth <c_duckworth@hotmail.com>
2023-08-14 10:58:20 +0100

    add happy path for get /api/topics

---
+++ __tests__/app.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
+const app = require("../app");
+const db = require("../db/connection");
+
+beforeEach(() => seed(testData));
+afterAll(() => db.end());
+
+describe("GET /api/items", () => {
+  it("responds with an array of topic objects, each of which should have properties slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        const { topics } = response.body;
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug");
+          expect(topic).toHaveProperty("description");
+        });
+        expect(topics).toHaveLength(3);
+      });
+  });
+});
---
+++ listen.js
@@ -0,0 +1,6 @@
+const app = require("./app")
+const port = 9090
+
+app.listen(9090, () => {
+  console.log("listening on 9090")
+})
