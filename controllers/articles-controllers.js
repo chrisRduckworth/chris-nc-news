e6fa18565cb7f1551801f1a4032267842a32bc64
chrisRduckworth <c_duckworth@hotmail.com>
2023-08-14 14:30:10 +0100

    refactor for nicer naming

--- controllers/articles-controllers.js
+++ controllers/articles-controllers.js
@@ -3,8 +3,8 @@ const { fetchArticleById } = require("../models/articles-models");
 exports.getArticleById = (req, res, next) => {
   const { article_id } = req.params;
   fetchArticleById(article_id)
-    .then((data) => {
-      res.status(200).send({ article: data });
+    .then((article) => {
+      res.status(200).send({ article });
     })
     .catch(next);
 };
