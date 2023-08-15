2b976dc60f3899a384d4f70df11da00c12648a7d
chrisRduckworth <c_duckworth@hotmail.com>
2023-08-15 14:31:44 +0100

    add error handling for DELETE /api/comments/:comment_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -161,7 +161,7 @@ describe("GET /api/articles/:article_id/comments", () => {
           "created_at",
           "author",
           "body",
-          "article_id"
+          "article_id",
         ];
         expect(comments).toHaveLength(11);
         expect(comments).toBeSortedBy("created_at", { descending: true });
@@ -241,39 +241,55 @@ describe("POST /api/articles/:article_id/comments", () => {
       .then(({ body }) => {
         expect(body.msg).toBe("Bad Request");
       });
-  })
+  });
   it("should respond with 400 Bad Request when send a malformed body", () => {
-    const newComment = {}
+    const newComment = {};
     return request(app)
       .post("/api/articles/1/comments")
       .send(newComment)
       .expect(400)
-      .then(({body}) => {
-        expect(body.msg).toBe("Bad Request")
-      })
-  })
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
   it("should respond with 400 Bad Request when send a body with invalid values", () => {
     const newComment = {
       body: "blah blah blah",
-      user_name: 0
-    }
+      user_name: 0,
+    };
     return request(app)
       .post("/api/articles/1/comments")
       .send(newComment)
       .expect(400)
-      .then(({body}) => {
-        expect(body.msg).toBe("Bad Request")
-      })
-  })
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad Request");
+      });
+  });
 });
 
-describe.only("DELETE /api/comments/:commend_it", () => {
-  it('should delete comment with given id, responds with status 204 no content', () => {
+describe("DELETE /api/comments/:commend_it", () => {
+  it("should delete comment with given id, responds with status 204 no content", () => {
     return request(app)
       .delete("/api/comments/1")
       .expect(204)
-      .then(({body})=> {
-        expect(body).toEqual({})
-      })
+      .then(({ body }) => {
+        expect(body).toEqual({});
+      });
   });
-})
+  it("should return 404 not found when given an id with no associated comment", () => {
+    return request(app)
+      .delete("/api/comments/500")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found");
+      });
+  });
+  it("should return 400 bad request when given an invalid id", () => {
+    return request(app)
+      .delete("/api/comments/banana")
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request");
+      });
+  });
+});
