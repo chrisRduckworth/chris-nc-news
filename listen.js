2c5ab84d9163a9ef7b2e289eac363c4ef2ce122d
chrisRduckworth <c_duckworth@hotmail.com>
2023-08-16 10:11:05 +0100

    changes for hosting

--- listen.js
+++ listen.js
@@ -1,6 +1,4 @@
-const app = require("./app")
-const port = 9090
+const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, () => {
-  console.log("listening on 9090")
-})
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
