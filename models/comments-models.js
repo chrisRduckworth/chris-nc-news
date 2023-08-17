5e01535c42d9f18e03cce7dd3678418521db0694
chrisRduckworth <c_duckworth@hotmail.com>
2023-08-17 10:17:58 +0100

    switch error handling to checkExists to make handling future errors easier

--- models/comments-models.js
+++ models/comments-models.js
@@ -17,16 +17,19 @@ exports.fetchCommentsByArticle = (articleId) => {
 exports.createComment = (reqBody, articleId) => {
   const { username } = reqBody;
   const { body } = reqBody;
-  return db
-    .query(
-      `INSERT INTO comments
+  return checkExists("articles", "article_id", articleId)
+    .then(() => {
+      return db.query(
+        `INSERT INTO comments
     (article_id, author, body)
     VALUES
     ($1, $2, $3)
     RETURNING *;
     `,
-      [articleId, username, body]
-    )
+        [articleId, username, body]
+      );
+    })
+
     .then(({ rows }) => {
       return rows[0];
     });
